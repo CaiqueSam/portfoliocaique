fd43ce7cc139d75dfa3559dcd5e036da30bb9cd0
CaiqueSmd <36681822+CaiqueSmd@users.noreply.github.com>
2018-06-17 10:26:21 -0300

    Delete scriptmotiongraphics.js

--- js/scriptmotiongraphics.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('.motiongraphics').css("background-color","#615AA0");
-	$('.motiongraphics').css("color","white");
-	$('.uxdesign').css("background-color","#615AA0");
-	$('.uxdesign').css("color","white");
-	$('.desenvolvimentoweb').css("background-color","#615AA0");
-	$('.desenvolvimentoweb').css("color","white");
-});
